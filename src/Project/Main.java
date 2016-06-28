2d137362990dc0d035df4ad644efdfde1dbec441
hwan_snut <SinChangHwan@users.noreply.github.com>
2016-06-28 19:46:37 +0900

    Add files via upload

---
+++ src/Project/Main.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame start = new MainFrame();
+		start.setVisible(true);
+	}
+
+}
